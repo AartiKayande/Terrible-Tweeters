2a6430d85ff282895b4daafb20ca12d9b79481e6
DESKTOP-UIT5GT6\DELL <aarti.andhale99@gmail.com>
2021-05-05 22:23:37 +0530

    first commit

---
+++ Assets/C# script/LevelController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelController : MonoBehaviour
+{
+    Monster[] _monsters;
+    [SerializeField] string _nextLevelName;
+
+    public Text TextLevel;
+   // [SerializeField] public GameObject Text;
+   // [SerializeField] public GameObject Image;
+    void OnEnable()
+    {
+        _monsters = FindObjectsOfType<Monster>();
+    }
+
+
+
+    void Update()
+    {
+        if (MonstersAreAllDead())
+
+
+            GoToNextLevel();
+    }
+     
+
+    void GoToNextLevel()
+    { 
+        Debug.Log("Go to level" + _nextLevelName);
+          
+            SceneManager.LoadScene(_nextLevelName);
+        
+    }
+     bool MonstersAreAllDead()
+    {
+        foreach(var monster in _monsters)
+        {
+            if (monster.gameObject.activeSelf)
+                return false;
+            // Text.SetActive(true);
+           // Image.SetActive(true);
+            
+        }
+        return true;
+    }
+}
